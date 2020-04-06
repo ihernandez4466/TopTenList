710d6dbae62b7de4675062b1cc76a7cef1100d63
Isabel Hernandez <isabelhernandez@isabels-mbp.attlocal.net>
2020-04-05 21:20:53 -0700

    Implemented functionality to function definitions

--- TopTenList.cpp
+++ TopTenList.cpp
@@ -2,20 +2,49 @@
 #include <string>
 #include "TopTenList.h"
 #include "Hyperlink.h"
-
+#include<iostream>
 using std::string;
 
 TopTenList::TopTenList()
-{
-
-}
+{}
 
 void TopTenList::set_at(int index, Hyperlink link)
 {
-
+    Hyperlink newLink;
+    newLink.text = link.text;
+    newLink.url = link.url;
+    _list.push_back(newLink);
 }
 
 Hyperlink TopTenList::get(int index)
 {
+    return _list[index];
+}
 
+void TopTenList::display_forward(){
+
+    if( _list.size() == 0 )
+    {
+        std::cout <<" is empty.\n";
+    }
+    for(int i=0; i <_list.size(); i++)
+    {
+        std::cout << i+1 << "." << _list[i].text << " "
+             <<_list[i].url << std::endl;
+    }
+}
+void TopTenList::display_backward(){
+    if( _list.size() == 0 )
+    {
+        std::cout <<" is empty.\n";
+    }
+    else
+    {
+        std::cout <<":\n";
+    }
+    for(int i = _list.size()-1; i >= 0; i++)
+    {
+        std::cout <<_list[i].text << " "
+             <<_list[i].url << std::endl;
+    }
 }
